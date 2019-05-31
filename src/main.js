569b8b3f0cd085aa8a084e48fe87b65eec045296
zhangzhe <zhangzhe@xyd.cn>
2019-05-31 10:55:10 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -5,13 +5,11 @@ import App from './App'
 import router from './router'
 import 'es6-promise/auto'
 import promise from 'es6-promise'
-import Api from './api/index.js'
 import Utils from './utils/index.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.prototype.$utils = Utils;
-Vue.prototype.$api = Api;
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
